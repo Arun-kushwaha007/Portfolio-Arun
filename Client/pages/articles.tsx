7e86b7e64b0c83760fadc4ce1df2a4a93189a74b
Arun <arunsk1310@gmail.com>
2025-04-26 19:29:49 +0530

    error fix4

--- Client/pages/articles.tsx
+++ Client/pages/articles.tsx
@@ -1,54 +1,65 @@
-import ArticleCard from '@/components/ArticleCard';
+// import ArticleCard from '@/components/ArticleCard';
 
-import { Article } from '@/types';
+// import { Article } from '@/types';
 
-import styles from '@/styles/ArticlesPage.module.css';
+// import styles from '@/styles/ArticlesPage.module.css';
 
-interface ArticlesPageProps {
-  articles: Article[];
-}
+// interface ArticlesPageProps {
+//   articles: Article[];
+// }
 
-const ArticlesPage = ({ articles }: ArticlesPageProps) => {
+// const ArticlesPage = ({ articles }: ArticlesPageProps) => {
+//   return (
+//     <div className={styles.layout}>
+//       <h1 className={styles.pageTitle}>My Articles</h1>
+//       <p className={styles.pageSubtitle}>
+//         Recent posts from{' '}
+//         <a
+//           href="https://dev.to/itsnitinr"
+//           target="_blank"
+//           rel="noopener"
+//           className={styles.underline}
+//         >
+//           dev.to
+//         </a>{' '}
+//         where I share insights and tutorials about web development.
+//       </p>
+//       <div className={styles.container}>
+//         {articles.map((article) => (
+//           <ArticleCard key={article.id} article={article} />
+//         ))}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export async function getStaticProps() {
+//   const res = await fetch(
+//     'https://dev.to/api/articles/me/published?per_page=6',
+//     {
+//       headers: {
+//         'api-key': process.env.DEV_TO_API_KEY!,
+//       },
+//     }
+//   );
+
+//   const data = await res.json();
+
+//   return {
+//     props: { title: 'Articles', articles: data },
+//     revalidate: 60,
+//   };
+// }
+
+// export default ArticlesPage;
+
+
+import React from 'react'
+
+const articles = () => {
   return (
-    <div className={styles.layout}>
-      <h1 className={styles.pageTitle}>My Articles</h1>
-      <p className={styles.pageSubtitle}>
-        Recent posts from{' '}
-        <a
-          href="https://dev.to/itsnitinr"
-          target="_blank"
-          rel="noopener"
-          className={styles.underline}
-        >
-          dev.to
-        </a>{' '}
-        where I share insights and tutorials about web development.
-      </p>
-      <div className={styles.container}>
-        {articles.map((article) => (
-          <ArticleCard key={article.id} article={article} />
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export async function getStaticProps() {
-  const res = await fetch(
-    'https://dev.to/api/articles/me/published?per_page=6',
-    {
-      headers: {
-        'api-key': process.env.DEV_TO_API_KEY!,
-      },
-    }
-  );
-
-  const data = await res.json();
-
-  return {
-    props: { title: 'Articles', articles: data },
-    revalidate: 60,
-  };
+    <div>articles</div>
+  )
 }
 
-export default ArticlesPage;
+export default articles
